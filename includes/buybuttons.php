7af63f02bcaf84c46d0dc2ddc51f0d4b26aea4bc
Luke Cavanagh <lukecav@users.noreply.github.com>
2017-04-17 13:29:53 -0600

    Update buybuttons.php

--- includes/buybuttons.php
+++ includes/buybuttons.php
@@ -305,14 +305,6 @@ function mbt_filter_amazon_buybutton_data($data, $store) {
 function mbt_amazon_buybutton_preview() {
 	if(empty($_REQUEST['data'])) { die(); }
 	$id = mbt_get_amazon_AISN($_REQUEST['data']);
-	if(mbt_is_genius_link($_REQUEST['data'])) {
-		echo('<span class="mbt_admin_message_success">'.__('Valid Genius Link', 'mybooktable').'</span>');
-	} else if(empty($id)) {
-		echo('<span class="mbt_admin_message_failure">'.__('Invalid Amazon product link', 'mybooktable').'</span>');
-	} else {
-		echo('<span class="mbt_admin_message_success">'.__('Valid Amazon product link', 'mybooktable').'</span>');
-	}
-	die();
 }
 
 function mbt_amazon_buybutton_editor($editor, $data, $id, $store) {
